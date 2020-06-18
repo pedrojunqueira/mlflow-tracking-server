545819e48ec7edeeddbe7cc5f4dc38a9e449edac
Pedro Junqueira <pedrojunqueira@MacBook-Pro.local>
2020-06-18 15:02:30 +0930

    initial commit

---
+++ mlflow/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# run.sh
+mlflow models serve -m $ARTIFACT_STORE -h $SERVER_HOST -p $SERVER_PORT --no-conda
---
+++ track_remote.py
@@ -0,0 +1,13 @@
+import mlflow
+remote_server_uri = "http://0.0.0.0:5555" # set to your server URI
+mlflow.set_tracking_uri(remote_server_uri)
+
+with mlflow.start_run():
+    mlflow.log_param("a", 1)
+    mlflow.log_metric("b", 2)
+
+    # Log an artifact (output file)
+    with open("output.txt", "w") as f:
+        f.write("Hello world!")
+    mlflow.log_artifact("output.txt")
+
